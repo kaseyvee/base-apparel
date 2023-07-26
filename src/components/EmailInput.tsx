46f859e5d38fa0f6768a707c759dbe7823526ef8
kethnal <kaseyvaldez@hotmail.com>
2023-07-26 15:39:09 -0700

    Animate send email

--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -34,52 +34,60 @@ function EmailInput() {
 
   return (
     <div>
-      {emailAccepted ? (
-        <p className="text-mauve text-base md:text-lg absolute">
-          You've been added to our mailing list!
-        </p>
-      ) : (
-        <AnimatePresence>
-          <motion.form
-            className="flex justify-center relative"
-            onSubmit={handleCheckValidity}
-            noValidate
-            initial={{opacity: 1, x:0 }}
-            exit={{ opacity: 0, x: 200 }}
+      <AnimatePresence mode="wait">
+        {emailAccepted ? (
+          <motion.p
+            className="text-mauve text-base text-center md:text-left md:text-lg h-[48px]"
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
           >
-            <input
-              className={`placeholder:text-mauve/50 outline-mauve text-black bg-white/0 border-solid ${
-                invalidInput ? "border-2" : "border"
-              } ${
-                invalidInput ? "border-red" : "border-mauve/50"
-              } rounded-full text-base md:text-lg w-full h-12 px-6`}
-              type="email"
-              placeholder="Email Address"
-              value={email}
-              onChange={handleInputChange}
-              required
-            />
-            <button
-              type="submit"
-              className="bg-gradient-to-br from-[#F8BFBF] to-pink shadow-xl shadow-mauve hover:brightness-125 focus:brightness-125 h-12 px-7 md:px-9 rounded-full absolute right-0 duration-200"
+            You've been added to our mailing list!
+          </motion.p>
+        ) : (
+          <motion.div
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 20 }}
+          >
+            <form
+              className="flex justify-center relative"
+              onSubmit={handleCheckValidity}
+              noValidate
             >
-              <Arrow />
-            </button>
-            <AnimatePresence>
-              {invalidInput && (
-                <motion.img
-                  className="absolute right-[100px] top-[25%]"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  src="icon-error.svg"
-                  alt=""
-                />
-              )}
-            </AnimatePresence>
-          </motion.form>
-        </AnimatePresence>
-      )}
+              <input
+                className={`placeholder:text-mauve/50 outline-mauve text-black bg-white/0 border-solid ${
+                  invalidInput ? "border-2" : "border"
+                } ${
+                  invalidInput ? "border-red" : "border-mauve/50"
+                } rounded-full text-base md:text-lg w-full h-12 px-6`}
+                type="email"
+                placeholder="Email Address"
+                value={email}
+                onChange={handleInputChange}
+                required
+              />
+              <button
+                type="submit"
+                className="bg-gradient-to-br from-[#F8BFBF] to-pink shadow-xl shadow-mauve hover:brightness-125 focus:brightness-125 h-12 px-7 md:px-9 rounded-full absolute right-0 duration-200"
+              >
+                <Arrow />
+              </button>
+              <AnimatePresence>
+                {invalidInput && (
+                  <motion.img
+                    className="absolute right-[100px] top-[25%]"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    src="icon-error.svg"
+                    alt=""
+                  />
+                )}
+              </AnimatePresence>
+            </form>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <AnimatePresence>
         {invalidInput && (
           <motion.p
